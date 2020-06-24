32569fdbc31eba75f9a903be0ea9f951fcf2a8a2
User Name <Email@example.com>
2020-06-24 15:16:04 +0300

    secend commit

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Colors.white;//Color(0xFFFFC12F);
+const kSecondaryColor = Color(0xff44c662);
+const kProductName = 'productName';
+const kProductPrice = 'productPrice';
+const kProductDescription = 'productDescription';
+const kProductLocation = 'productLocation';
+const kProductCategory = 'productCategory';
+const kProductsCollection = 'Products';
+const kUnActiveColor = Color(0xFFC1BDB8);
+const kJackets = 'jackets';
+const kTrousers = 'trousers';
+const kTshirts = 't-shirts';
+const kShoes = 'shoes';
+const kOrders = 'Orders';
+const kOrderDetails = 'OrderDetails';
+const kTotallPrice = 'TotallPrice';
+const kAddress = 'Address';
+const kProductQuantity = 'Quantity';
